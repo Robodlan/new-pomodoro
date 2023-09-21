2d0c2c7f4c4aa36bc37013e9986d71aa93326242
Reinaldo <reinaldo.robles@icloud.com>
2023-09-20 20:33:12 -0400

    script modified

--- script.js
+++ script.js
@@ -1,163 +1,81 @@
-let timeLeft = document.getElementById('time-left')
-const showWork = document.getElementById('work')
 // const sesionValue = document.getElementById('sesion-value')
-const startStop = document.getElementById('start-stop')
 // const timeBreak = document.getElementById('time-break')
 // const addBreak = document.getElementById('add-break')
 // const lessBreak = document.getElementById('less-break')
-const addWork = document.getElementById('add-work')
 // const  lessWork = document.getElementById('less-work')
+
+const timeLeft = document.getElementById('time-left')
+const showWork = document.getElementById('work')
+const startStop = document.getElementById('start-stop')
+// const addWork = document.getElementById('add-work')
 const shortBreak = document.getElementById('short-break')
-const longBreak = document.getElementById('long-break')
+// const longBreak = document.getElementById('long-break')
 let currentLabel = document.getElementById('timer-label')
 let audio = new Audio('click.mp3')
+let lastAlert = new Audio('chimes.mp3')
 
 let timeOut
-let isRunning = false
-let breakLong = 15
-let breakPLus = 5
-let plusTime = 25
-let workTime =  25 * 60
-let breakTime = 5 * 60
-let currentTime = workTime
+let isRunning = true
+let workTime =  1 * 60
+let breakTime = 2 * 60
+// let currentTime = workTime
 
-
-
-// addWork.addEventListener('click', add)
-// lessWork.addEventListener('click', less)
-// addBreak.addEventListener('click', addBreakFun)
-// lessBreak.addEventListener('click', lessBreakFun)
-shortBreak.addEventListener('click', displayShortBreak)
-longBreak.addEventListener('click', displayLongBreak)
-showWork.addEventListener('click', displayWork)
-
-function displayWork() {
-    audio.play()
-    currentLabel.innerHTML = 'Work'
-    workTime = 25 * 60
-    currentTime =  workTime
-    displayUpdate()
-}
-
-function displayShortBreak() {
-    audio.play()
-    currentLabel.innerHTML = 'Break'
-    currentTime = breakTime
-    displayUpdate()
-}
-
-function displayLongBreak() {
-    audio.play()
-    currentLabel.innerHTML = 'Long Break'
-    workTime = 15 * 60
-    currentTime = workTime
-    displayUpdate()
-}
-
-
-// function add() {
-//     if (displayWork) {
-//         currentTime++
-//     } else if (displayShortBreak) {
-//         breakPLus++
-//         currentTime = `${breakPLus}` * 60
-//         currentTime++
-//         // timeLeft.textContent = makingTime(currentTime)
-//         // displayUpdate()
-//     } else {
-//         breakLong++
-//         currentTime = `${breakLong}` * 60
-//         let x = currentTime++
-//         timeLeft.textContent = makingTime(x)
-//         displayUpdate()
-//     }
-  
-// }
-
-function less() {
-    plusTime-=1
-    workTime = `${plusTime}` * 60
-    currentTime = workTime
-    timeLeft.textContent = makingTime(currentTime)
-}
-
-// function addBreakFun() {
-//    breakPLus += 1
-//    breakTime = `${breakPLus}` * 60
-//    currentTime = breakTime
-//    timeBreak.textContent = makingTime(currentTime)
-// }
-
-// function lessBreakFun() {
-//     breakPLus -= 1
-//     breakTime = `${breakPLus}` * 60
-//     currentTime = breakTime
-//     timeBreak.textContent = makingTime(currentTime)
-// }
-
-
-function makingTime(seconds) {
-    const minutes = Math.floor(seconds / 60)
-    const remainingSeconds = seconds % 60
-    const formatingMinutes = String(minutes).padStart(2, '0')
-    const  formatingSeconds = String(remainingSeconds).padStart(2, '0')
-    return `${formatingMinutes} : ${formatingSeconds}`
-}
-
-function displayUpdate() {
-    timeLeft.textContent = makingTime(currentTime)
-    // currentLabel.innerHTML = 'Work'
-}
 
 function toggleTimer() {
-    audio.play()
-    if (isRunning) {
-        clearInterval(timeOut)
+    // audio.play()
+    if (!timeOut) {
+        timeOut = setInterval(updateTime, 100)
         startStop.innerHTML = 'Start'
     } else {
-       timeOut = setInterval(updateTime, 1000)      
-       startStop.innerHTML = 'Stop'
-    }
-    isRunning = !isRunning
- }
-
- function updateTime() {
-    if (currentTime > 0 ) {
-        // currentLabel.innerHTML = 'Work' 
-        // currentLabel
-        currentTime--
-        displayUpdate()
-        
-    }  else {
-        if (currentLabel.innerHTML = 'Work' && currentTime === 0) {
-            workTime = 5 * 60
-            currentTime = workTime
-            currentTime--
-            // breakTime--
-            // currentTime = breakTime
-            // currentTime--
-            currentLabel.innerHTML = 'Break'
-            // timeLeft.textContent = 25S
-            timeBreak.innerHTML = makingTime(currentTime)
-            // displayUpdate()
-         }
-        // } else {
-        //     currentLabel = 'Work'
-        //     currentTime = workTime
-        // }
-        // displayUpdate()
+        clearInterval(timeOut)
+        timeOut = null
+        startStop.innerHTML = 'Resume'
     }
  }
 
  function reset() {
     audio.play()
     clearInterval(timeOut)
-    isRunning = false
-    workTime =  25 * 60
-    currentTime = workTime
-    document.getElementById('start-stop').innerHTML = 'Start'
-    displayUpdate()
- }
+    timeOut = null
+   if (isRunning) {
+    timeLeft.textContent = '25:00'
+   } else {
+    timeLeft.textContent = '05:00'
+   }
+   startStop.innerHTML = 'Start'
+}
+
+ function updateTime() {
+   if (workTime === 0 && breakTime === 0) {
+        alertSound()
+        reset()
+        isRunning = !isRunning
+        if (isRunning && workTime > 0) {
+            timeLeft.textContent = '25:00'
+        } else {
+            timeLeft.textContent = '05:00'
+        }
+    } 
+      else {
+      if (isRunning && workTime > 0){
+        workTime--
+      } else  {
+          timeLeft.textContent = breakTime--
+          breakTime--
+          currentLabel.innerHTML = 'Break'
+          console.log(breakTime)
+    }
+      const minutes = Math.floor(isRunning ? workTime / 60 : breakTime / 60)
+      const seconds = isRunning ? workTime % 60 : breakTime % 60
+      timeLeft.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+   }
+}  
+
+function alertSound() {
+  lastAlert.play()
+}
+ 
+
 
  document.getElementById('start-stop').addEventListener('click', toggleTimer)
  document.getElementById('reset').addEventListener('click', reset)
