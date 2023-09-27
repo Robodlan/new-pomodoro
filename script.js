860312683d9a2bef24c3edec4d41e87f5044aec7
Reinaldo <reinaldo.robles@icloud.com>
2023-09-27 19:35:31 -0400

    fixing script

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const timeLeft = document.getElementById('time-left')
 const startStop = document.getElementById('start-stop')
+const addWorkBtn = document.getElementById('add-work-btn')
+const lessWorkBtn = document.getElementById('less-work-btn')
+const addBreakBtn = document.getElementById('add-break-btn')
+const lessBreakBtn = document.getElementById('less-break-btn')
 const addWorkInput = document.getElementById('add-work')
 const addBreakInput = document.getElementById('add-break')
 let currentLabel = document.getElementById('timer-label')
@@ -16,78 +20,91 @@ let breakSeconds = 60
 let workTime =  `${workMinutes}` * `${workSeconds}`
 let breakTime = `${breakMinutes}` * `${breakSeconds}`
 
-// function addWork(){
-//   if (!timeOut && startStop.innerHTML != 'Resume' ) {
-//     currentLabel.innerHTML = "Work"
-//      workMinutes += 1 
-//      addWorkInput.value = workMinutes
-//      workTime =  `${workMinutes}` * `${workSeconds}`
-//      timeLeft.textContent = formatTime(workTime)
-//   } else {
-//    workMinutes = 25  
-//  }
-// }
+function addWork(){
+  if (!timeOut ) {
+     currentLabel.innerHTML = "Work"
+     workMinutes += 1 
+     addWorkInput.value = workMinutes
+     workTime =  `${workMinutes}` * `${workSeconds}`
+     timeLeft.textContent = formatTime(workTime)
+  } else {
+   workMinutes = 25  
+ }
+}
 
-// function lessWork() {
-//   if (!timeOut && startStop.innerHTML != 'Resume') {
-//     currentLabel.innerHTML = "Work"
-//     workMinutes -= 1
-//     addWorkInput.value = workMinutes
-//     workTime =  `${workMinutes}` * `${workSeconds}`
-//     timeLeft.textContent = formatTime(workTime)
-//   } else {
-//     workMinutes = 25
-//   }
-// }
+function lessWork() {
+  if (!timeOut && startStop.innerHTML != 'Resume') {
+    currentLabel.innerHTML = "Work"
+    workMinutes -= 1
+    addWorkInput.value = workMinutes
+    workTime =  `${workMinutes}` * `${workSeconds}`
+    timeLeft.textContent = formatTime(workTime)
+  } else {
+    workMinutes = 25
+  }
+}
 
-// function addBreak(){
-//   if(!timeOut && startStop.innerHTML != 'Resume') { 
-//      currentLabel.innerHTML = " ⬆ Break"
-//      breakMinutes += 1
-//      addBreakInput.value = breakMinutes
-//      breakTime = `${breakMinutes}` * `${breakSeconds}`
-//      timeLeft.textContent = formatTime(breakTime)
-//   } else {
-//     currentLabel.innerHTML = "Work"
-//     timeLeft.textContent = formatTime(workTime)
-//   }
-// }
+function addBreak(){
+  if(!timeOut && startStop.innerHTML != 'Resume') { 
+     currentLabel.innerHTML = " ⬆ Break"
+     breakMinutes += 1
+     addBreakInput.value = breakMinutes
+     breakTime = `${breakMinutes}` * `${breakSeconds}`
+     timeLeft.textContent = formatTime(breakTime)
+  } else {
+    currentLabel.innerHTML = "Work"
+    timeLeft.textContent = formatTime(workTime)
+  }
+}
 
-// function lessBreak() {
-//   if(!timeOut && startStop.innerHTML != 'Resume') {
-//     currentLabel.innerHTML = " ↓ Break"
-//     breakMinutes -= 1
-//     addBreakInput.value = breakMinutes
-//     breakTime = `${breakMinutes}` * `${breakSeconds}`
-//     timeLeft.textContent = formatTime(breakTime)
-//   } else {
-//     currentLabel.innerHTML = 'Work'
-//     timeLeft.textContent = formatTime(workTime)
-//   }
-// }
+function lessBreak() {
+  if(!timeOut && startStop.innerHTML != 'Resume') {
+    currentLabel.innerHTML = " ↓ Break"
+    breakMinutes -= 1
+    addBreakInput.value = breakMinutes
+    breakTime = `${breakMinutes}` * `${breakSeconds}`
+    timeLeft.textContent = formatTime(breakTime)
+  } else {
+    currentLabel.innerHTML = 'Work'
+    timeLeft.textContent = formatTime(workTime)
+  }
+}
 
 
 function toggleTimer() {
+  addWorkBtn.setAttribute('disabled', '')
+  lessWorkBtn.setAttribute('disabled', '')
+  addBreakBtn.setAttribute('disabled', '')
+  lessBreakBtn.setAttribute('disabled', '')
 if (!timeOut || !isRunning) {
+      workMinutes = 25
+      breakMinutes = 5
       audio.play()
       isRunning = true
-      timeOut = setInterval(updateTime, 1)
+      timeOut = setInterval(updateTime, 10)
       timeLeft.textContent = formatTime(workTime) 
       startStop.innerHTML = 'Pause'
       currentLabel.innerHTML = 'Work'
-        addWorkInput.value = ''
-        addBreakInput.value = ''
+      addWorkInput.value = ''
+      addBreakInput.value = ''
     } else {
         clearInterval(timeOut)
         timeOut = null
         startStop.innerHTML = 'Resume'
-        currentLabel.innerHTML = 'Work'
-        addWorkInput.value = ''
-        addBreakInput.value = ''
+        // currentLabel.innerHTML = 'Work'
+        // addWorkInput.value = ''
+        // addBreakInput.value = ''
     }
  }
 
  function reset() {
+    workMinutes = 25
+    breakMinutes = 5
+    startStop.removeAttribute('disabled','')
+    addWorkBtn.removeAttribute('disabled', '')
+    lessWorkBtn.removeAttribute('disabled', '')
+    addBreakBtn.removeAttribute('disabled', '')
+    lessBreakBtn.removeAttribute('disabled', '')
     addWorkInput.value = ''
     addBreakInput.value = ''
     currentLabel.innerHTML = 'Work'
@@ -116,8 +133,9 @@ if (!timeOut || !isRunning) {
       currentLabel.innerHTML = 'Done!'
       startStop.innerHTML = 'Start'
       clearInterval(timeOut)
-      // lastAlert.play()
-      document.getElementById('start-stop').disabled
+      lastAlert.play()
+      startStop.innerHTML = 'Press Reset'
+      startStop.setAttribute('disabled', '')
     } 
   }
   timeLeft.textContent = formatTime(isRunning ? workTime : breakTime)
@@ -130,12 +148,12 @@ function formatTime(seconds) {
   return `${minutes.toString().padStart(2, '0')} : ${remainingSeconds.toString().padStart(2, '0')}`
 }
 
- document.getElementById('start-stop').addEventListener('click', toggleTimer)
- document.getElementById('reset').addEventListener('click', reset)
-//  document.getElementById('add-work-btn').addEventListener('click', addWork)
-//  document.getElementById('less-work-btn').addEventListener('click', lessWork)
-//  document.getElementById('add-break-btn').addEventListener('click', addBreak)
-//  document.getElementById('less-break-btn').addEventListener('click', lessBreak)
+startStop.addEventListener('click', toggleTimer)
+document.getElementById('reset').addEventListener('click', reset)
+addWorkBtn.addEventListener('click', addWork)
+lessWorkBtn.addEventListener('click', lessWork)
+addBreakBtn.addEventListener('click', addBreak)
+lessBreakBtn.addEventListener('click', lessBreak)
 
 
  
